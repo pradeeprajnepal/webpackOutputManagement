84d7ecd8cafbbd8a7ea41e15892074fcbc7da21a
Pradeep Raj Nepal <pradeepxwon@gmail.com>
2022-02-10 01:13:55 +0545

    webpack-outputManagement-demo

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin= require ('html-webpack-plugin');
+
+
+// js loader
+module.exports = {
+  entry: {
+    index:'./src/index.js',
+    print:'./src/print.js',
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      title:'Output Management',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean:true,
+  },
+
+
+};
